a5af0a6926b4bc86eeef1a9b811116b2a9fa9cbd
gustavo <fdfjonfmd@gmail.com>
2023-05-29 16:02:35 -0300

    init

---
+++ App.jsx
@@ -0,0 +1,5 @@
+import HomeScreen from './src/components/HomeScreen';
+
+export default function App() {
+  return <HomeScreen />;
+};
---
+++ src/components/HomeScreen.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+import styles from '../utils/globals';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
